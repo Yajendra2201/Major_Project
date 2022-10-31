c0cd555f0790278b07d3a3df4c843fd5bd074727
Yajendra_Prajapati <86663172+Yajendra2201@users.noreply.github.com>
2022-10-31 23:02:38 +0530

    Update textfiletobinaryfile.py

--- CryptoCode1/textfiletobinaryfile.py
+++ CryptoCode1/textfiletobinaryfile.py
@@ -1,6 +1,6 @@
 #-,bullet dots of list, 's or 't
 def TxtToBin(n):
-    lo='CryptoCode\\UploadF\\'+n
+    lo='CryptoCode1\\UploadF\\'+n
     with open(lo,'r') as txtfile:
         mytextstring = txtfile.read()
 
@@ -8,5 +8,5 @@ def TxtToBin(n):
 
     binarray = ' '.join(format(ch, 'b') for ch in bytearray(mytextstring))
 
-    with open('CryptoCode\F2\BinfileName1.bin', 'w') as binfile:
+    with open('CryptoCode1\F2\BinfileName1.bin', 'w') as binfile:
         binfile.write(binarray)
