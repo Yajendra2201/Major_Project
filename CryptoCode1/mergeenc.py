07a3c5e80057b5058f5e02b190003cc33da2cb64
Yajendra_Prajapati <86663172+Yajendra2201@users.noreply.github.com>
2022-10-31 23:03:33 +0530

    Update mergeenc.py

--- CryptoCode1/mergeenc.py
+++ CryptoCode1/mergeenc.py
@@ -1,9 +1,9 @@
 import os
 
 def MergeIn1(n):
-    f3=open('CryptoCode\F2\BinfileName11.bin','rb')
-    f5=open('CryptoCode\F2\BinfileName12.bin','rb')
-    f6=open('CryptoCode\F2\BinfileName13.bin','rb')
+    f3=open('CryptoCode1\F2\BinfileName11.bin','rb')
+    f5=open('CryptoCode1\F2\BinfileName12.bin','rb')
+    f6=open('CryptoCode1\F2\BinfileName13.bin','rb')
 
     s=b''
     for i in f3:
@@ -15,7 +15,7 @@ def MergeIn1(n):
     for i in f6:
         s+=i
 
-    lo='CryptoCode\\encFile\\'+n+'.bin'
+    lo='CryptoCode1\\encFile\\'+n+'.bin'
 
     f4=open(lo,'wb')
     f4.write(s)
@@ -25,6 +25,6 @@ def MergeIn1(n):
     f5.close()
     f6.close()
 
-    os.remove("CryptoCode\F2\BinfileName11.bin")
-    os.remove("CryptoCode\F2\BinfileName12.bin")
-    os.remove("CryptoCode\F2\BinfileName13.bin")
+    os.remove("CryptoCode1\F2\BinfileName11.bin")
+    os.remove("CryptoCode1\F2\BinfileName12.bin")
+    os.remove("CryptoCode1\F2\BinfileName13.bin")
